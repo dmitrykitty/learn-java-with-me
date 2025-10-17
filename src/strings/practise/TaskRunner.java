784fbb7b68379ef191727bfa0c7b68708ea85bee
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-10-17 17:56:19 +0200

    getUnique char method added

---
+++ src/strings/practise/TaskRunner.java
@@ -0,0 +1,11 @@
+package strings.practise;
+
+public class TaskRunner {
+    void main(){
+        System.out.println(Polindrom.isPolindrom("abbaabba"));
+        System.out.println(Polindrom.isPolindrom2("abbaabba"));
+
+
+        System.out.println(UniqueChars.getUniqueChars("abc Cpddd Dio OsfWw"));
+    }
+}
