eb6de274d7978b7d66d25aed5091d04afdf1b4d3
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-10-20 13:15:56 +0200

    d

--- src/oop/enums/EnumRunner.java
+++ src/oop/enums/EnumRunner.java
@@ -1,4 +1,17 @@
 package oop.enums;
 
+import java.util.Arrays;
+
 public class EnumRunner {
+    public static void main(String[] args) {
+        ProcessorType type = ProcessorType.INTEL; //mamy do wyboru tylko dwie opcji, ktore są w enum
+        System.out.println(type);
+        System.out.println(type.name());
+        System.out.println(ProcessorType.valueOf("AMD"));
+        System.out.println(ProcessorType.AMD.ordinal()); //dostac index tej konkretnej wartosci enuma
+
+        System.out.println(Arrays.toString(ProcessorType.values())); //dostac wszystkie wartosci enuma
+
+        System.out.println(ProcessorType.INTEL.getName());
+    }
 }
