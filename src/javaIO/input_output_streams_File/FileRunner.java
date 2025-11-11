bc82b3e5709d6261f75ef9153b477f908e6fc61f
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-11-11 12:19:25 +0100

    started with javaIO

---
+++ src/javaIO/input_output_streams_File/FileRunner.java
@@ -0,0 +1,35 @@
+package javaIO.input_output_streams_File;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileRunner {
+    static void main() throws IOException {
+        File file = new File("resources/test.txt");
+
+        System.out.println(file.createNewFile()); //tworzenie pliku - true jezeli stworzono i nie istnial wczesniej
+
+        System.out.println(file.exists());
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
+        System.out.println(file.getName());
+        System.out.println(file.getParent());
+        System.out.println(file.length()); //size
+        //can execute() - czy mozemy wykonywac
+        //can read() - czy mozemy odczytywac
+        //can write() - czy mozemy zmieniac
+
+        System.out.println(file.canExecute());
+        System.out.println(file.getAbsolutePath()); //D:\study\AGH\JAVA_NEW\course_2\resources\test.txt
+
+        File dir = new File("resources/test/dir");
+
+        if (dir.mkdirs()) {
+            System.out.println("Directories " + dir + " were successfully created ");
+        } else {
+            System.out.println("Error creating directories");
+        }
+        String[] list = dir.list(/*filter*/); //-> zwraca array z nazw plikow
+
+    }
+}
