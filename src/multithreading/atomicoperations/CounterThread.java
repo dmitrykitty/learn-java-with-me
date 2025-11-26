81835e75f48c7a54db9987f1e139616190df20ea
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-11-26 23:52:43 +0100

    threading: start - added

---
+++ src/multithreading/atomicoperations/CounterThread.java
@@ -0,0 +1,16 @@
+package multithreading.atomicoperations;
+
+public class CounterThread implements Runnable {
+    private final Counter counter;
+
+    public CounterThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
---
+++ src/multithreading/threadrunnable/SimpleThread.java
@@ -0,0 +1,10 @@
+package multithreading.threadrunnable;
+
+public class SimpleThread extends Thread{
+
+    @Override
+    //to samo co main tylko dla tego threadu
+    public void run() {
+        System.out.println("Hello " + getName());
+    }
+}
