564da638c36dc37f2e92b16971cc82acb6a43f26
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-12-19 20:13:24 +0100

    statement and preparedStatement using added

--- java-jdbc/src/main/java/com/dnikitin/jdbc/intro/JdbcRunner.java
+++ java-jdbc/src/main/java/com/dnikitin/jdbc/intro/JdbcRunner.java
@@ -2,8 +2,9 @@ package com.dnikitin.jdbc.intro;
 
 import com.dnikitin.jdbc.util.ConnectionManager;
 
-import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     static void main() throws SQLException {
@@ -17,6 +18,16 @@ public class JdbcRunner {
                     id serial primary key,
                     data text not null
                 )
+                """;
+        String sql3 = """
+                insert into info(data) values
+                ('test1'),
+                ('test2'),
+                ('test3');
+                """;
+        String sql4 = """
+                select *
+                from authors; 
                 """;
 
 
@@ -32,6 +43,36 @@ public class JdbcRunner {
             //return true if returned object is ResultSet (so only for select queries)
             //execute mainly used for DDL operations as creating or droping something
             System.out.println(execute);//false, because Create executed
+
+            // ---------------[EXECUTEUPDATE]-----------------
+            //var executeUpdate = statement.executeUpdate(sql3);//return amount of changes/inserted rows
+            //System.out.println(executeUpdate);
+
+            //to get index of inserted element
+            String sql5 = """
+                    insert into info(data) values
+                    ('testautogen');
+                    """;
+            statement.executeUpdate(sql5, Statement.RETURN_GENERATED_KEYS);
+            ResultSet keys = statement.getGeneratedKeys();
+            if(keys.next()){
+                System.out.println("index" + keys.getInt("id"));
+            }
+
+
+            // ---------------[EXECUTEQUERY]-----------------
+            connection.setSchema("book_storage");
+            ResultSet resultSet = statement.executeQuery(sql4);//almost like Iterator. Closed with statement
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getInt("id")
+                        + ": "
+                        + resultSet.getString("first_name")
+                        + " "
+                        + resultSet.getString("last_name"));
+            }
+
+
         }
 
     }
---
+++ java-jdbc/src/main/java/com/dnikitin/jdbc/sqlinjection/SqlInjectionExample.java
@@ -0,0 +1,98 @@
+package com.dnikitin.jdbc.sqlinjection;
+
+import com.dnikitin.jdbc.util.ConnectionManager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlInjectionExample {
+    static void main() throws SQLException {
+
+        String authorId = "3";
+        List<Integer> booksByAuthorId = getBookByAuthorId(authorId); //[5, 6]
+        System.out.println(booksByAuthorId);
+
+        //problem - we can modify authorId string, to get all books, or even drop some tables
+        authorId = "3 or 1 = 1";
+        booksByAuthorId = getBookByAuthorId(authorId);
+        System.out.println(booksByAuthorId); //[1, 3, 4, 5, 6, 9, 10, 11, 2]
+
+
+        int intAuthorId = 3;
+        booksByAuthorId = getBookByAuthorIdWithPrepareStatement(intAuthorId);
+        System.out.println(booksByAuthorId);
+
+        List<Integer> booksWithPagesFrom150To300 = getBookWherePagesBetween(150, 300);
+        System.out.println(booksWithPagesFrom150To300); //[3, 11, 10, 9]
+    }
+
+    private static List<Integer> getBookByAuthorId(String authorId) throws SQLException {
+        try (Connection connection = ConnectionManager.openConnection();
+             Statement statement = connection.createStatement()) {
+
+            connection.setSchema("book_storage");
+
+            String sql = """
+                    select id 
+                    from books
+                    where author_id = %s
+                    """.formatted(authorId);
+
+            ResultSet resultSet = statement.executeQuery(sql);
+            List<Integer> bookIds = new ArrayList<>();
+            while (resultSet.next()) {
+                bookIds.add(resultSet.getInt("id"));
+                //if value could be null
+                //bookIds.add(resultSet.getObject("id", Integer.class));  NULL SAFE
+            }
+            return bookIds;
+        }
+    }
+
+    //prepareStatement implements Statement so has all its functionality
+    //has a lot of set methods to provide type of value
+    private static List<Integer> getBookByAuthorIdWithPrepareStatement(int authorId) throws SQLException {
+        String sql = """
+                select id 
+                from books
+                where author_id = ? --add question to put param 
+                """;
+        try (Connection connection = ConnectionManager.openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            connection.setSchema("book_storage");
+            preparedStatement.setInt(1, authorId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Integer> bookIds = new ArrayList<>();
+            while (resultSet.next()) {
+                bookIds.add(resultSet.getInt("id"));
+            }
+            return bookIds;
+
+        }
+    }
+
+    private static List<Integer> getBookWherePagesBetween(int left, int right) throws SQLException {
+        String sql = """
+                select id 
+                from books
+                where pages_amount between ? and ?
+                order by pages_amount desc
+                """;
+        try (Connection connection = ConnectionManager.openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            connection.setSchema("book_storage");
+            preparedStatement.setInt(1, left);
+            preparedStatement.setInt(2, right);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Integer> bookIds = new ArrayList<>();
+            while (resultSet.next()) {
+                bookIds.add(resultSet.getInt("id"));
+            }
+            return bookIds;
+        }
+    }
+}
