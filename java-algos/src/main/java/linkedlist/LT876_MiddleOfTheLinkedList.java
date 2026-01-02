a3ad73658958a70dc5c4775b71fa0ba684359162
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-01-02 15:31:00 +0100

    lt2095 - removing middle node - added

--- java-algos/src/main/java/linkedlist/LT876_MiddleOfTheLinkedList.java
+++ java-algos/src/main/java/linkedlist/LT876_MiddleOfTheLinkedList.java
@@ -3,52 +3,36 @@ package linkedlist;
 /**
  * Given the head of a singly linked list, return the middle node of the linked list.
  * If there are two middle nodes, return the second middle node.
-
+ * <p>
  * Example 1:
  * Input: head = [1,2,3,4,5]
  * Output: [3,4,5]
  * Explanation: The middle node of the list is node 3.
-
+ * <p>
  * Example 2:
  * Input: head = [1,2,3,4,5,6]
  * Output: [4,5,6]
  * Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
  */
 public class LT876_MiddleOfTheLinkedList {
-    public static class ListNode {
-        int val;
-        ListNode next;
+    /**
+     * SOLUTION STARTS HERE!!!
+     * Based on fast and slow pointers
+     *
+     * @param head of the list
+     * @return middle of the list
+     */
+    public ListNode middleNode(ListNode head) {
 
-        ListNode() {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-        /**
-         * SOLUTION STARTS HERE!!!
-         * Based on fast and slow pointers
-         *
-         * @param head of the list
-         * @return middle of the list
-         */
-        public ListNode middleNode(ListNode head) {
-
-            ListNode slow = head;
-            ListNode fast = head;
-
-            while (fast != null && fast.next != null) {
-                slow = slow.next;
-                fast = fast.next.next;
-            }
-            return slow;
-        }
+        return slow;
     }
 }
+
 
