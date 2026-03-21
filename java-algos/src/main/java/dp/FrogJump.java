387dd65bc9532ca5328fd250d8334436fcb1e4d0
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-03-21 10:54:32 +0100

    add extended solutions for Frog Jump problem: variable step size and optimized approaches using dynamic programming

--- java-algos/src/main/java/dp/FrogJump.java
+++ java-algos/src/main/java/dp/FrogJump.java
@@ -15,18 +15,22 @@ public class FrogJump {
 
     private int inner(int[] mem, int[] height, int n) {
         if (n == 0) return mem[n] = 0;
-        if (n == 1) return mem[n] = Math.abs(height[n] - height[n - 1]);
+
 
         if (mem[n] != -1) return mem[n];
-        return mem[n] = Math.min(
-                inner(mem, height, n - 1) + Math.abs(height[n] - height[n - 1]),
-                inner(mem, height, n - 2) + Math.abs(height[n] - height[n - 2]));
+        int left = inner(mem, height, n - 1) + Math.abs(height[n] - height[n - 1]);
+        //to remove one step
+        int right = Integer.MAX_VALUE;
+
+        if(n > 1){
+            right = inner(mem, height, n - 2) + Math.abs(height[n] - height[n - 2]);
+        }
+        return mem[n] = Math.min(left, right);
 
     }
 
     public int frogJumpTab(int n, int[] heights) {
         if(n <= 1) return 0;
-        if(n == 2) return Math.abs(heights[1] - heights[0]);
 
         int[] dp = new int[n];
         dp[0] = 0;
@@ -43,6 +47,7 @@ public class FrogJump {
 
     public int frogJumpOpt(int n, int[] heights) {
         if(n <= 1) return 0;
+
         int a = 0;
         int b = Math.abs(heights[1] - heights[0]);
 
@@ -54,6 +59,53 @@ public class FrogJump {
             b = temp;
         }
         return b;
+    }
+
+    public int frogJumpWithKSteps(int n, int[] heights, int k) {
+        if(n <= 1) return 0;
+
+        int[] dp =  new int[n];
+        dp[0] = 0;
+
+        for(int i = 1; i < Math.min(k, n - 1); i++) {
+            dp[i] = Math.abs(heights[i] - heights[0]);
+        }
+
+        if(n - 1 <= k){
+            return dp[n - 1];
+        }
+
+        for (int i = 2; i < n; i++) {
+            int start = Math.max(0, i - k);
+            int curMin = Integer.MAX_VALUE;
+
+            for(int j = start; j < i; j++) {
+                curMin = Math.min(curMin, dp[j] + Math.abs(heights[i] - heights[j]));
+            }
+
+            dp[i] = curMin;
+        }
+
+        return dp[n - 1];
+    }
+
+    public int frogJumpWithKStepsOpt(int n, int[] heights, int k) {
+        if(n <= 1) return 0;
+
+        int[] dp =  new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            int start = Math.max(0, i - k);
+
+            for(int j = start; j < i; j++) {
+                int cost = dp[j] + Math.abs(heights[i] - heights[j]);
+                dp[i] =  Math.min(cost, dp[i]);
+            }
+        }
+
+        return dp[n - 1];
     }
 
     static void main() {
